f7fb3956c11eb942869b356f9ee6a9c39f3ba329
Victor Chin <victor@objectmage.com>
2017-02-08 08:52:11 +0100

    Add Files

---
+++ PhotoSharingApplication/Controllers/PhotoController.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+using System.Globalization;
+using PhotoSharingApplication.Models;
+using PhotoSharingApplication.LocationCheckService;
+using System.IO;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.RetryPolicies;
+using System.Configuration;
+using System.Diagnostics;
+
+namespace PhotoSharingApplication.Controllers
+{
+    [HandleError(View = "Error")]
+    [ValueReporter]
+    public class PhotoController : Controller
+    {
+        private IPhotoSharingContext context;
+
+        //Constructors
+        public PhotoController()
+        {
+            context = new PhotoSharingContext();
+        }
+
+        public PhotoController(IPhotoSharingContext Context)
+        {
+            context = Context;
+        }
+
+        //
+        // GET: /Photo/
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        [ChildActionOnly]
+        public ActionResult _PhotoGallery(int number = 0)
+        {
+            List<Photo> photos;
+
+            if (number == 0)
+            {
+                photos = context.Photos.ToList();
+            }
+            else
+            {
+                photos = (from p in context.Photos
+                          orderby p.CreatedDate descending
+                          select p).Take(number).ToList();
+            }
+
+            return PartialView("_PhotoGallery", photos);
+        }
+
+        public ActionResult Display(int id)
+        {
+            Photo photo = context.FindPhotoById(id);
+            if (photo == null)
+            {
+                return HttpNotFound();
+            }
+            return View("Display", photo);
+        }
+
+        public ActionResult DisplayByTitle(string title)
+        {
+            Photo photo = context.FindPhotoByTitle(title);
+            if (photo == null)
+            {
+                return HttpNotFound();
+            }
+            return View("Display", photo);
+        }
+
+        [Authorize]
+        public ActionResult Create()
+        {
+            Photo newPhoto = new Photo();
+            newPhoto.UserName = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(User.Identity.Name);
+            newPhoto.CreatedDate = DateTime.Today;
+            return View("Create", newPhoto);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(Photo photo, HttpPostedFileBase image)
+        {
+            photo.UserName = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(User.Identity.Name);
+            photo.CreatedDate = DateTime.Today;
+            if (photo.Location != "")
+            {
+                string stringLatLong = CheckLocation(photo.Location);
+                if (stringLatLong.StartsWith("Success"))
+                {
+                    char[] splitChars = {':'};
+                    string[] coordinates = stringLatLong.Split(splitChars);
+                    photo.Latitude = coordinates[1];
+                    photo.Longitude = coordinates[2];
+                }
+            }
+            if (!ModelState.IsValid)
+            {
+                return View("Create", photo);
+            }
+            else
+            {
+                if (image != null)
+                {
+                    photo.ImageMimeType = image.ContentType;
+                    photo.PhotoFile = new byte[image.ContentLength];
+                    photo.AzurePath = uploadFromStream(image.InputStream,image.FileName);
+                }
+                context.Add<Photo>(photo);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+
+        private string uploadFromStream(Stream inputStream,string filename)
+        {
+            CloudStorageAccount storageAccount =
+                CloudStorageAccount.Parse(ConfigurationManager.AppSettings["StorageConnStr"]);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference("photosharing");
+            try
+            {
+                BlobRequestOptions requestOptions = new BlobRequestOptions() { RetryPolicy = new NoRetry() };
+                container.CreateIfNotExistsAsync(requestOptions, null).Wait();
+
+                BlobContainerPermissions permissions = container.GetPermissions();
+                permissions.PublicAccess = BlobContainerPublicAccessType.Container;
+                container.SetPermissions(permissions);
+
+                CloudBlockBlob blockBlob = container.GetBlockBlobReference(filename);
+                blockBlob.Properties.ContentType = "image/jpeg";
+                blockBlob.UploadFromStream(inputStream);
+                return blockBlob.Uri.ToString();
+            }
+            catch (StorageException)
+            {
+                Trace.WriteLine("If you are running with the default connection string, please make sure you have started the storage emulator. Press the Windows key and type Azure Storage to select and run it from the list of applications - then restart the sample.");
+                throw;
+            }
+        }
+
+        [Authorize]
+        public ActionResult Delete(int id)
+        {
+            Photo photo = context.FindPhotoById(id);
+            if (photo == null)
+            {
+                return HttpNotFound();
+            }
+            return View("Delete", photo);
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Photo photo = context.FindPhotoById(id);
+            context.Delete<Photo>(photo);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult GetImage(int id)
+        {
+            Photo photo = context.FindPhotoById(id);
+            if (photo != null)
+            {
+                return Content(photo.AzurePath);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public ActionResult SlideShow()
+        {
+            return View("SlideShow", context.Photos.ToList());
+        }
+
+        public ActionResult FavoritesSlideshow()
+        {
+            List<Photo> favPhotos = new List<Photo>();
+            List<int> favoriteIds = Session["Favorites"] as List<int>;
+            if (favoriteIds == null)
+            {
+                favoriteIds = new List<int>();
+            }
+            Photo currentPhoto;
+
+            foreach (int favID in favoriteIds)
+            {
+                currentPhoto = context.FindPhotoById(favID);
+                if (currentPhoto != null)
+                {
+                    favPhotos.Add(currentPhoto);
+                }
+            }
+
+            return View("SlideShow", favPhotos);
+        }
+
+        public ContentResult AddFavorite(int PhotoID)
+        {
+            List<int> favorites = Session["Favorites"] as List<int>;
+            if (favorites == null)
+            {
+                favorites = new List<int>();
+            }
+            favorites.Add(PhotoID);
+            Session["Favorites"] = favorites;
+            return Content("The picture has been added to your favorites", "text/plain", System.Text.Encoding.Default);
+        }
+
+        private string CheckLocation(string location)
+        {
+            LocationCheckServiceClient client = null;
+            string response = "";
+            try
+            {
+                client = new LocationCheckServiceClient();
+                response = client.GetLocation(location);
+            }
+            catch (Exception e)
+            {
+                response = "Error: " + e.Message;
+            }
+            return response;
+        }
+
+        public ViewResult Map()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Chat(int id)
+        {
+            Photo photo = context.FindPhotoById(id);
+            if (photo == null)
+            {
+                return HttpNotFound();
+            }
+            return View("Chat", photo);
+        }
+    }
+}
---
+++ PhotoSharingApplication/Models/PhotoSharingInitializer.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.RetryPolicies;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace PhotoSharingApplication.Models
+{
+    public class PhotoSharingInitializer : CreateDatabaseIfNotExists<PhotoSharingContext>
+    {
+        //This method puts sample data into the database
+        protected override void Seed(PhotoSharingContext context)
+        {
+            base.Seed(context);
+
+            //Create some photos
+            var photos = new List<Photo>
+            {
+                new Photo {
+                    Title = "Sample Photo 1",
+                    Description = "This is the first sample photo in the Adventure Works photo application",
+                    UserName = "AllisonBrown",
+                    //PhotoFile = getFileBytes("\\Images\\flower.jpg"),
+                    AzurePath = storeInAzure("Images\\flower.jpg"),
+                    ImageMimeType = "image/jpeg",
+                    CreatedDate = DateTime.Today.AddDays(-5),
+                    Location = "Paris, France",
+                    Latitude = "48.85693",
+                    Longitude = "2.3412"
+                },
+                new Photo {
+                    Title = "Sample Photo 2",
+                    Description = "This is the second sample photo in the Adventure Works photo application",
+                    UserName = "RogerLengel",
+                    //PhotoFile = getFileBytes("\\Images\\orchard.jpg"),
+                    ImageMimeType = "image/jpeg",
+                    AzurePath = storeInAzure("Images\\orchard.jpg"),
+                    CreatedDate = DateTime.Today.AddDays(-14),
+                    Location = "London, UK",
+                    Latitude = "51.506321",
+                    Longitude = "-0.12714"
+                },
+                new Photo {
+                    Title = "Sample Photo 3",
+                    Description = "This is the third sample photo in the Adventure Works photo application",
+                    UserName = "AllisonBrown",
+                    //PhotoFile = getFileBytes("\\Images\\path.jpg"),
+                    ImageMimeType = "image/jpeg",
+                    AzurePath = storeInAzure("Images\\path.jpg"),
+                    CreatedDate = DateTime.Today.AddDays(-14),
+                    Location = "Tokyo, Japan",
+                    Latitude = "35.683208",
+                    Longitude = "139.808945"
+                }
+            };
+            photos.ForEach(s => context.Photos.Add(s));
+            context.SaveChanges();
+
+            //Create some comments
+            var comments = new List<Comment>
+            {
+                new Comment {
+                    PhotoID = 1,
+                    UserName = "JamieStark",
+                    Subject = "Sample Comment 1",
+                    Body = "This is the first sample comment in the Adventure Works photo application"
+                },
+                new Comment {
+                    PhotoID = 1,
+                    UserName = "JimCorbin",
+                    Subject = "Sample Comment 2",
+                    Body = "This is the second sample comment in the Adventure Works photo application"
+                },
+                new Comment {
+                    PhotoID = 3,
+                    UserName = "RogerLengel",
+                    Subject = "Sample Comment 3",
+                    Body = "This is the third sample photo in the Adventure Works photo application"
+                }
+            };
+            comments.ForEach(s => context.Comments.Add(s));
+            context.SaveChanges();
+        }
+
+        private string storeInAzure(string ImageToUpload)
+        {
+            CloudStorageAccount storageAccount=
+                CloudStorageAccount.Parse(ConfigurationManager.AppSettings["StorageConnStr"]);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference("photosharing");
+            try
+            {
+                BlobRequestOptions requestOptions = new BlobRequestOptions() { RetryPolicy = new NoRetry() };
+                container.CreateIfNotExistsAsync(requestOptions, null).Wait();
+
+                BlobContainerPermissions permissions = container.GetPermissions();
+                permissions.PublicAccess = BlobContainerPublicAccessType.Container;
+                container.SetPermissions(permissions);
+
+                CloudBlockBlob blockBlob = container.GetBlockBlobReference(ImageToUpload);
+                blockBlob.Properties.ContentType = "image/jpeg";
+                blockBlob.UploadFromFileAsync(HttpRuntime.AppDomainAppPath + ImageToUpload).Wait();
+                return blockBlob.Uri.ToString();
+            }
+            catch (StorageException)
+            {
+                Trace.WriteLine("If you are running with the default connection string, please make sure you have started the storage emulator. Press the Windows key and type Azure Storage to select and run it from the list of applications - then restart the sample.");
+                throw;
+            }
+
+
+        }
+
+        //This gets a byte array for a file at the path specified
+        //The path is relative to the route of the web site
+        //It is used to seed images
+        private byte[] getFileBytes(string path)
+        {
+            FileStream fileOnDisk = new FileStream(HttpRuntime.AppDomainAppPath + path, FileMode.Open);
+            byte[] fileBytes;
+            using (BinaryReader br = new BinaryReader(fileOnDisk))
+            {
+                fileBytes = br.ReadBytes((int)fileOnDisk.Length);
+            }
+            return fileBytes;
+        }
+
+    }
+}
